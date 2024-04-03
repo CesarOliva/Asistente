05e7f86e78b3e428b711c8330f1b151038bc7703
Cesar Oliva <cesaroliva.work@gmail.com>
2024-04-03 15:57:49 -0600

    Agregando enviar mensajes

--- sarahi_gui.py
+++ sarahi_gui.py
@@ -11,6 +11,9 @@ import subprocess as sub
 import os
 from tkinter import *
 from PIL import Image, ImageTk
+import pyautogui as at
+import webbrowser
+import time
 
 comandos = """
             Comandos que puedes usar:
@@ -19,6 +22,7 @@ comandos = """
             - Abre (pagina web o app)
             - Alarma a las (hora en 24H)
             - Escribe (nota)
+            - Mensaje (contacto)
             - Termina
 """
 
@@ -53,24 +57,24 @@ window_photo = Label(main_window, image=photo)
 window_photo.pack(pady=5)
 
 def open_w_pages():
-    window_apps = Toplevel()
-    window_apps.title("Acciones Páginas")
-    window_apps.configure(bg="#FAD7A0")
-    window_apps.geometry("300x200")
-    window_apps.resizable(0, 0)
-    main_window.eval(f'tk::PlaceWindow {str(window_apps)} center')
+    window_pages = Toplevel()
+    window_pages.title("Acciones Páginas")
+    window_pages.configure(bg="#FAD7A0")
+    window_pages.geometry("300x200")
+    window_pages.resizable(0, 0)
+    main_window.eval(f'tk::PlaceWindow {str(window_pages)} center')
 
-    title_label = Label(window_apps, text="Páginas", fg="white", bg="#FAD7A0",
+    title_label = Label(window_pages, text="Páginas", fg="white", bg="#FAD7A0",
                         font=("Consolas", "15", "bold"))
     title_label.pack(pady=5)
 
-    button_add = Button(window_apps, text="Lista", fg="white", bg="#149FE0",
+    button_add = Button(window_pages, text="Lista", fg="white", bg="#149FE0",
                         font=('Arial', '10', 'bold'), command=talk_pages)
     button_add.pack(pady=4)
-    button_add = Button(window_apps, text="Agregar Páginas", fg="white", bg="#149FE0",
+    button_add = Button(window_pages, text="Agregar Páginas", fg="white", bg="#149FE0",
                         font=('Arial', '10', 'bold'), command=open_add_pages)
     button_add.pack(pady=4)
-    button_add = Button(window_apps, text="Eliminar lista", fg="white", bg="#149FE0",
+    button_add = Button(window_pages, text="Eliminar lista", fg="white", bg="#149FE0",
                         font=('Arial', '10', 'bold'), command=delete_pages)
     button_add.pack(pady=4)
 
@@ -200,6 +204,80 @@ def add_apps():
     nameapp_entry.delete(0, "end")
     pathapp_entry.delete(0, "end")
 
+def open_w_contacts():
+    window_contacts = Toplevel()
+    window_contacts.title("Acciones Contactos")
+    window_contacts.configure(bg="#FAD7A0")
+    window_contacts.geometry("300x200")
+    window_contacts.resizable(0, 0)
+    main_window.eval(f'tk::PlaceWindow {str(window_contacts)} center')
+
+    title_label = Label(window_contacts, text="Contactos", fg="white", bg="#FAD7A0",
+                        font=("Consolas", "15", "bold"))
+    title_label.pack(pady=5)
+
+    button_add = Button(window_contacts, text="Lista", fg="white", bg="#149FE0",
+                        font=('Arial', '10', 'bold'), command=talk_contacts)
+    button_add.pack(pady=4)
+    button_add = Button(window_contacts, text="Agregar Contactos", fg="white", bg="#149FE0",
+                        font=('Arial', '10', 'bold'), command=open_add_contacts)
+    button_add.pack(pady=4)
+    button_add = Button(window_contacts, text="Eliminar lista", fg="white", bg="#149FE0",
+                        font=('Arial', '10', 'bold'), command=delete_contacts)
+    button_add.pack(pady=4)
+
+def talk_contacts():
+    if bool(contacts) == True:
+        text = "Has agregado los siguientes contactos"
+        filename = "added.mp3"
+        talk(text, filename)
+        for contact in contacts:
+            filename = "app.mp3"
+            talk(contact, filename)
+    else:
+        text = "Aún no has agregado nada"
+        filename = "notyet.mp3"
+        talk(text, filename)
+
+def open_add_contacts():
+    global namecontact_entry, numbercontact_entry
+    window_contacts = Toplevel()
+    window_contacts.title("Agregar Contactos")
+    window_contacts.configure(bg="#434343")
+    window_contacts.geometry("300x200")
+    window_contacts.resizable(0, 0)
+    main_window.eval(f'tk::PlaceWindow {str(window_contacts)} center')
+
+    title_label = Label(window_contacts, text="Agrega un contacto", fg="white", bg="#434343",
+                        font=("Arial", "15", "bold"))
+    title_label.pack(pady=3)
+
+    name_label = Label(window_contacts, text="Nombre del contacto", fg="white", bg="#434343",
+                        font=("Arial", "10"))
+    name_label.pack(pady=2)
+    namecontact_entry = Entry(window_contacts, width=20)
+    namecontact_entry.pack(pady=1)
+
+    number_label = Label(window_contacts, text="Número del contacto (+51)", fg="white", bg="#434343",
+                        font=("Arial", "10"))
+    number_label.pack(pady=2)
+    numbercontact_entry = Entry(window_contacts, width=25)
+    numbercontact_entry.pack(pady=1)
+
+    save_button = Button(window_contacts, text="Guardar", bg="#16222A", fg="white", width=8, height=1, command=add_contacts)
+    save_button.pack(pady=4)
+
+def add_contacts():
+    name_contact = namecontact_entry.get().strip()
+    number_contact = numbercontact_entry.get().strip()
+    sites[name_contact] = number_contact
+    save_data(name_contact, number_contact, "contacts.txt")
+    namecontact_entry.delete(0, "end")
+    numbercontact_entry.delete(0, "end")
+
+def delete_contacts():
+    os.remove('contacts.txt')
+
 def charge_data(name_dict, name_file):
     try:
         with open(name_file) as f:
@@ -222,7 +300,8 @@ sites=dict()
 charge_data(sites, "pages.txt")
 programs=dict()
 charge_data(programs, "programs.txt")
-
+contacts=dict()
+charge_data(contacts, "contacts.txt")
 
 def listen():
     listener = sr.Recognizer()
@@ -324,13 +403,29 @@ def abre(rec):
         filename = "not_added.mp3"
         talk(text, filename)
 
-def escribe():
-    try:
-        with open('media/pendientes.txt', 'a') as f:
-            write(f)
-    except FileNotFoundError as e:
-        file = open('media/pendientes.txt', 'w')
-        write(file)
+def enviar_mensaje(rec):
+    text = "¿A quién se enviará el mensaje?"
+    filename = "message.mp3"
+    talk(text, filename)
+    contact = listen()
+    contact = contact.strip()
+    print(contact)
+    if contact in contacts:
+        for cont in contacts:
+            if cont == contact:
+                contact = contacts[cont]
+                text = "¿Qué mensaje deseas enviar?"
+                filename = "wantForSend.mp3"
+                talk(text, filename)
+                message = listen()
+                webbrowser.open(f'https://web.whatsapp.com/send?phone={contact}&text={message}')
+                time.sleep(15)
+                at.press('enter')
+    else:
+        text = "Contacto no registrado"
+        filename = "not_added.mp3"
+        talk(text, filename)
+
 
 def func_alarma(rec):
     thread = tr.Thread(target=clock, args=(rec,))
@@ -339,13 +434,24 @@ def func_alarma(rec):
 def stop_alarma(rec):
     mixer.music.stop()
 
+def cierra(rec):
+    # for program in programs:
+    #     kill_program = programs[program].split('\\')
+    #     kill_program = kill_program[-1]
+    #     if program in rec:
+    #         text = ""
+    #         filename = ""
+    #         talk(text, filename)
+    pass
+
 key_words = {
     'reproduce': reproduce,
     'busca': busca,
     'alarma': func_alarma,
     'detente': stop_alarma,
     'abre': abre,
-    'escribe': escribe
+    'mensaje': enviar_mensaje,
+    'cierra': cierra
 }
 
 def run_sarahi():
@@ -362,6 +468,13 @@ def run_sarahi():
         if 'busca' in rec:
             key_words['busca'](rec)
             break
+        elif 'escribe' in rec:
+            try:
+                with open('media/pendientes.txt', 'a') as f:
+                    write(f)
+            except FileNotFoundError as e:
+                file = open('media/pendientes.txt', 'w')
+                write(file)
         else:
             for word in key_words:
                 if word in rec:
@@ -390,5 +503,9 @@ button_pages.place(x=720, y=150, width=120, height=30)
 button_apps = Button(main_window, text="Programas", fg="white", bg="#149FE0",
                         font=('Arial', '10', 'bold'), command=open_w_apps)
 button_apps.place(x=720, y=200, width=120, height=30)
+
+button_apps = Button(main_window, text="Contactos", fg="white", bg="#149FE0",
+                        font=('Arial', '10', 'bold'), command=open_w_contacts)
+button_apps.place(x=720, y=250, width=120, height=30)
 
 main_window.mainloop()
