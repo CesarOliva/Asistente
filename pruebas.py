dd3267bdfef8738a663e44c0465e2cdbfb47f9e9
Cesar Oliva <cesaroliva.work@gmail.com>
2024-04-26 19:07:48 -0600

    Continuando Google Calendar

--- pruebas.py
+++ pruebas.py
@@ -13,3 +13,35 @@
 # if input != condicion:
 #     stop_threads = True
 # print('thread killed')
+
+
+start_date = input("Fecha de inicio: ")
+#26 de febrero a las 13:58
+start_date = start_date.replace(' de ', ' ')
+start_date = start_date.replace(' a las ', ' ')
+print(start_date)
+start_date = start_date.split(' ')
+print(start_date)
+
+
+months = {
+    "enero": "01",
+    "febrero": "02",
+    "marzo": "03",
+    "abril": "04",
+    "mayo": "05",
+    "junio": "06",
+    "julio": "07",
+    "agosto": "08",
+    "septiembre": "09",
+    "octubre": "10",
+    "noviembre": "11",
+    "diciembre": "12"
+}
+
+for month in months:
+    if start_date[1] in months:
+        month = months[start_date[1]]
+
+new_date = f"2024-{month}-{start_date[0]} {start_date[2]}"
+print(new_date)
